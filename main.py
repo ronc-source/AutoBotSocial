b24bb32fbb0d4d0aa795b6287a06a7734ead9d23
Ronc-Source <ronniecharlesbusiness@outlook.com>
2024-12-12 16:03:10 -0500

    Setup automated video downloading from reddit with yaml configurations

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
 import praw
-import requests
-import moviepy
+from RedDownloader import RedDownloader
 import yaml
 from yaml import Loader
 
-# Reddit API credentials
-stream = open("credentials.yaml", 'r')
+# Load YAML configuration
+stream = open("configuration.yaml", 'r')
 dictionary = yaml.load(stream, Loader=Loader)
+
+# Reddit API credentials
 CLIENT_ID = dictionary['client_id']
 CLIENT_SECRET = dictionary['client_secret']
 USER_AGENT = dictionary['user_agent']
 
 # Directory to save videos from Reddit
-DOWNLOAD_DIR = "downloads"
+DOWNLOAD_DIR = dictionary['download_dir']
 
 # SubReddit for videos
-SUBREDDIT = "discordVideos"
+SUBREDDIT = dictionary['subreddit']
 
 
 def redditInstance():
@@ -23,16 +24,20 @@ def redditInstance():
                        client_secret=CLIENT_SECRET, 
                        user_agent=USER_AGENT)
 
-def getLatestVideo(redditAccess):
+def findLatestVideoPost(redditAccess):
     subredditAccess = redditAccess.subreddit(SUBREDDIT)
     for post in subredditAccess.hot():
         if post.is_video:
             print(f"Found video titled: {post.title}")
             return post
 
+def downloadVideoPost(post):
+    RedDownloader.Download(post.url, output=post.title, destination=DOWNLOAD_DIR)
+
 def main():
     redditAccess = redditInstance()
-    video = getLatestVideo(redditAccess)
+    post = findLatestVideoPost(redditAccess)
+    downloadVideoPost(post)
     
 
 if __name__ == "__main__":
